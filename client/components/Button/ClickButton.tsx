25aaf7fc776b7f6a236e2c401a69d727db8d6c97
Roshan <nkroshankumar@gmail.com>
2023-01-13 18:49:02 +0530

    custom button created

---
+++ client/components/Button/ClickButton.tsx
@@ -0,0 +1,64 @@
+import { cva, VariantProps } from "class-variance-authority"
+import { ReactNode } from "react"
+
+export type ClickButtonProp = VariantProps<typeof buttonClass> & {
+    onClick: () => void
+    children: ReactNode
+}
+
+const buttonClass = cva(
+    [
+        "px-10 py-3 rounded-md outline-0 text-xl md:text-2xl font-semibold shadow-lg shadow-black hover:scale-110 active:scale-95 transition-all",
+    ],
+    {
+        variants: {
+            variant: {
+                primary: ["bg-dPri text-white "],
+                secondary: ["bg-black text-dPri"],
+            },
+
+            width: {
+                true: ["w-full"],
+            },
+
+            disabled: {
+                true: [""],
+            },
+        },
+
+        defaultVariants: {
+            variant: "primary",
+            disabled: false,
+        },
+
+        compoundVariants: [
+            {
+                variant: "primary",
+                disabled: true,
+                className: [
+                    "bg-dPri/50 hover:scale-100 active:scale-100 cursor-not-allowed text-gray-400",
+                ],
+            },
+        ],
+    }
+)
+
+const ClickButton = ({
+    variant,
+    width,
+    disabled = false,
+    onClick,
+    children,
+}: ClickButtonProp) => {
+    return (
+        <button
+            disabled={disabled as boolean}
+            className={buttonClass({ variant, width, disabled })}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default ClickButton

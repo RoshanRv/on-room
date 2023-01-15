66abc0e8c8fb28a1a4cb3e725c6937984e839c68
RoshanRv <nkroshankumar@gmail.com>
2023-01-15 14:33:18 +0530

    schema for user created

---
+++ server/src/schema/users.schema.ts
@@ -0,0 +1,14 @@
+import { object, string, nativeEnum, TypeOf } from "zod"
+import { Role } from "@prisma/client"
+
+export const createUserSchema = object({
+    body: object({
+        name: string().min(1, "Name is Required"),
+        email: string().email().min(1, "Email is Required"),
+        password: string().min(1, "Password is Required"),
+        img: string().min(1, "Img is Required").url(),
+        role: nativeEnum(Role),
+    }),
+})
+
+export type createUserSchemaType = TypeOf<typeof createUserSchema>

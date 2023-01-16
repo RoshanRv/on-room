411ac1a62b256694f699b01dd77596efb5cdd45f
RoshanRv <nkroshankumar@gmail.com>
2023-01-16 14:07:50 +0530

    configured server and client

---
+++ server/config/default.ts
@@ -0,0 +1,7 @@
+export default {
+    origin: "http://localhost:3001",
+    port: 3002,
+    accessTokenLife: "20m",
+    refreshTokenLife: "1y",
+    sameSite: "strict",
+}
---
+++ server/config/production.ts
@@ -0,0 +1,7 @@
+export default {
+    origin: " ",
+    port: process.env.PORT,
+    accessTokenLife: "20m",
+    refreshTokenLife: "1y",
+    sameSite: "none",
+}

7636e10705783343e34d5de89fa4395b4ab03d4e
RoshanRv <nkroshankumar@gmail.com>
2023-01-16 14:06:38 +0530

    created route for new sessions

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,11 @@ import { Express, Request, Response } from "express"
 import validateInput from "./middleware/validateInput"
 import { createUserSchema } from "./schema/users.schema"
 import { createUserHandler } from "./controller/users.controller"
+import { createSessionSchema } from "./schema/sessions.schema"
+import {
+    createSessionHandler,
+    getCurrentUserHandler,
+} from "./controller/sessions.contoller"
 
 const routes = (app: Express) => {
     app.get("/healthcheck", (req: Request, res: Response) => {
@@ -10,6 +15,15 @@ const routes = (app: Express) => {
 
     //  Create New User
     app.post("/api/users", validateInput(createUserSchema), createUserHandler)
+
+    // create new session / login
+    app.post(
+        "/api/sessions",
+        validateInput(createSessionSchema),
+        createSessionHandler
+    )
+
+    app.get("/api/me", getCurrentUserHandler)
 }
 
 export default routes

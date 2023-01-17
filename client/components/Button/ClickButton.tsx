8f85a1e82cc670b36767b8c033898e751bd1f83e
RoshanRv <nkroshankumar@gmail.com>
2023-01-17 18:08:11 +0530

    added size in btn

--- client/components/Button/ClickButton.tsx
+++ client/components/Button/ClickButton.tsx
@@ -1,4 +1,5 @@
-import { cva, VariantProps } from "class-variance-authority"
+import buttonClass from "@utils/buttonClass"
+import { VariantProps } from "class-variance-authority"
 import { ReactNode } from "react"
 
 export type ClickButtonProp = VariantProps<typeof buttonClass> & {
@@ -6,41 +7,6 @@ export type ClickButtonProp = VariantProps<typeof buttonClass> & {
     children: ReactNode
     isLoading?: boolean
 }
-
-const buttonClass = cva(
-    [
-        "px-10 md:py-3 py-2 rounded-md outline-0 text-xl md:text-2xl font-semibold shadow-lg shadow-black/50 hover:scale-105 active:scale-95 transition-all",
-    ],
-    {
-        variants: {
-            variant: {
-                primary: ["bg-dPri text-white "],
-                secondary: ["bg-black text-dPri"],
-            },
-
-            width: {
-                true: ["w-full"],
-            },
-
-            disabled: {
-                true: ["hover:scale-100 active:scale-100 cursor-not-allowed"],
-            },
-        },
-
-        defaultVariants: {
-            variant: "primary",
-            disabled: false,
-        },
-
-        compoundVariants: [
-            {
-                variant: "primary",
-                disabled: true,
-                className: ["bg-dPri/50  text-gray-400"],
-            },
-        ],
-    }
-)
 
 const ClickButton = ({
     variant,

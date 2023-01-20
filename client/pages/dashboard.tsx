0df35c23a5872feea2f46855e9b680a1140cd0c9
RoshanRv <nkroshankumar@gmail.com>
2023-01-20 23:38:53 +0530

    dashboard updated

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -1,9 +1,28 @@
 import axios from "axios"
-import React from "react"
-import { getCookies, setCookie } from "cookies-next"
+import React, { useEffect, useState } from "react"
+import {
+    dehydrate,
+    QueryClient,
+    useQuery,
+    QueryObserver,
+    useQueryClient,
+} from "@tanstack/react-query"
+import { useRouter } from "next/navigation"
+import { UserResponse } from "@components/Header/Header"
+import { queryClient } from "./_app"
 
-const dashboard = ({ user }: { user: UserProps }) => {
-    console.log(user)
+const dashboard = () => {
+    const router = useRouter()
+
+    const { data: user, isError } = useQuery<UserResponse>({
+        queryKey: ["users"],
+        queryFn: () =>
+            axios.get(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/api/me`, {
+                withCredentials: true,
+            }),
+    })
+
+    if (isError) router.push("/signin")
 
     return <div>dashboard</div>
 }
@@ -18,14 +37,14 @@ export const getServerSideProps = async ({
     req: any
 }) => {
     try {
-        const user = await axios.get(
-            `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/api/me`,
-            {
+        const user = await queryClient.fetchQuery(["users"], () =>
+            axios.get(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/api/me`, {
                 headers: {
                     Cookie: req.headers.cookie,
                 },
-            }
+            })
         )
+
         return {
             props: {
                 user: user.data,

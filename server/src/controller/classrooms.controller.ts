1104a5d2d91c179efea787ec8bc904b9227cca8e
RoshanRv <nkroshankumar@gmail.com>
2023-02-06 23:54:42 +0530

    getClassroomHander updated and handleEnrollClassroom func created

--- server/src/controller/classrooms.controller.ts
+++ server/src/controller/classrooms.controller.ts
@@ -1,6 +1,13 @@
 import { createClassroomSchemaType } from "@schema/classroom.schema"
-import { createClassroom, getClassroom } from "@service/classrooms.services"
+import {
+    createClassroom,
+    enrollClassroom,
+    getClassroomByTeacherId,
+    getClassrooms,
+} from "@service/classrooms.services"
+import { log } from "console"
 import { Request, Response } from "express"
+import { omit } from "lodash"
 
 export const createClassroomHandler = async (
     req: Request<{}, {}, createClassroomSchemaType["body"]>,
@@ -24,10 +31,30 @@ export const createClassroomHandler = async (
 export const getClassroomHandler = async (req: Request, res: Response) => {
     try {
         const { user } = res.locals
-        const classroom = await getClassroom(user.id as string)
-
-        return res.status(200).send(classroom)
+        if (user.role === "teacher") {
+            const classroom = await getClassroomByTeacherId(user.id as string)
+            return res.status(200).send(classroom)
+        } else if (user.role === "student") {
+            const classroom = await getClassrooms()
+            return res.status(200).send(classroom)
+        }
     } catch (e: any) {
+        return res.status(400).send(e.message)
+    }
+}
+
+export const handleEnrollClassroom = async (req: Request, res: Response) => {
+    try {
+        const { user } = res.locals
+        const { id } = req.body
+        console.log(req.body)
+
+        const student = omit(user, ["iat", "exp", "sessionId"])
+        const classroom = await enrollClassroom(id, student)
+        return res.status(201).send(classroom)
+    } catch (e: any) {
+        console.log(e)
+
         return res.status(400).send(e.message)
     }
 }

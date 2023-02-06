dc386cb61a3992e85d5c7416bca0335e5ab88a74
RoshanRv <nkroshankumar@gmail.com>
2023-02-06 23:53:43 +0530

    updateUserHandler func created

--- server/src/controller/users.controller.ts
+++ server/src/controller/users.controller.ts
@@ -1,6 +1,13 @@
 import { Request, Response } from "express"
-import { createUserSchemaType } from "@schema/users.schema"
-import { createUser, findUserByEmail } from "@service/users.services"
+import {
+    createUserSchemaType,
+    updateUserSchemaType,
+} from "@schema/users.schema"
+import {
+    createUser,
+    findUserByEmail,
+    updateUserById,
+} from "@service/users.services"
 import { hashSync } from "bcryptjs"
 import { omit } from "lodash"
 
@@ -21,6 +28,24 @@ export const createUserHandler = async (
         const newUser = await createUser(body)
 
         return res.status(201).send(omit(newUser, ["password"]))
+    } catch (e: any) {
+        console.log(e)
+        return res.status(400).send(e.message)
+    }
+}
+
+export const updateUserHandler = async (
+    req: Request<{}, {}, updateUserSchemaType["body"]>,
+    res: Response
+) => {
+    try {
+        const { body } = req
+        const { user } = res.locals
+
+        console.log(res.locals)
+
+        const updatedUser = await updateUserById(user.id as string, body)
+        return res.status(201).send(updatedUser)
     } catch (e: any) {
         console.log(e)
         return res.status(400).send(e.message)

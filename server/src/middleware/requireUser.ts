93799163a2dcf3870e983b2f5d532142e73e545b
RoshanRv <nkroshankumar@gmail.com>
2023-02-08 19:21:03 +0530

    added requireStudent

--- server/src/middleware/requireUser.ts
+++ server/src/middleware/requireUser.ts
@@ -3,7 +3,7 @@ import { Response, Request, NextFunction } from "express"
 const requireUser = (req: Request, res: Response, next: NextFunction) => {
     const { user } = res.locals
 
-    if (!user) res.status(401).send("Valid Token Not Found")
+    if (!user) return res.status(401).send("Valid Token Not Found")
     return next()
 }
 
@@ -15,6 +15,16 @@ export const requireTeacher = (
     next: NextFunction
 ) => {
     const { user } = res.locals
-    if (user.role === "student") res.status(403).send("Teachers Only")
+    if (user.role === "student") return res.status(403).send("Teachers Only")
+    return next()
+}
+
+export const requireStudent = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const { user } = res.locals
+    if (user.role === "teacher") return res.status(403).send("Students Only")
     return next()
 }

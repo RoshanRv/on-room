dca70d2bcbddaf0c0d7bc8dd34d2557145de44e9
RoshanRv <nkroshankumar@gmail.com>
2023-02-21 23:50:48 +0530

    added img to empty wrapper

--- client/components/EmptyWrapper/EmptyWrapper.tsx
+++ client/components/EmptyWrapper/EmptyWrapper.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode } from "react"
 import { isEmpty } from "lodash"
+import Image from "next/image"
+import WRAPPER from "@public/wrapper_img.png"
+import TEACHER_IMG from "@public/teacher_role.jpeg"
 
 interface Props {
     data: any
@@ -16,7 +19,14 @@ const EmptyWrapper = ({ data, children, noDataText }: Props) => {
                 children
             ) : (
                 <div className="my-8 text-center text-xl">
-                    <h1>{noDataText}</h1>
+                    <h1 className="text-gray-800 dark:text-gray-300">
+                        {noDataText}
+                    </h1>
+                    <Image
+                        src={WRAPPER}
+                        alt="Wrapper_Image"
+                        className="mx-auto"
+                    />
                 </div>
             )}
         </>

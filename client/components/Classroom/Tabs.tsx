e163f2980e62ebaabb6e8a76f60307f8dbae9fea
RoshanRv <nkroshankumar@gmail.com>
2023-02-25 23:41:48 +0530

    added announcement

--- client/components/Classroom/Tabs.tsx
+++ client/components/Classroom/Tabs.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { MdOutlineAssignment } from "react-icons/md"
 import { HiUser } from "react-icons/hi"
+import { IoMdMegaphone } from "react-icons/io"
 
 interface Props {
     tab: Tabs
@@ -17,17 +18,22 @@ const tabList = [
         name: "students",
         icon: <HiUser />,
     },
+    {
+        name: "announcements",
+        icon: <IoMdMegaphone />,
+    },
 ]
 
 const Tabs = ({ tab, setTab }: Props) => {
     return (
-        <aside className="flex h-full flex-col gap-y-6 md:w-3/12 xl:w-2/12 text-lg md:text-xl  text-gray-600 dark:text-gray-400 border-r-2 border-dPri/60 ">
+        <aside className="flex h-full flex-col gap-y-2 md:w-3/12 xl:w-2/12 text-lg md:text-xl  text-gray-600 dark:text-gray-400 border-r-2 border-dPri/60 pr-2">
             {tabList.map((tabDetail, i) => (
                 <div
+                    key={i}
                     onClick={() => setTab(tabDetail.name as Tabs)}
                     className={`capitalize transition-all cursor-pointer ${
                         tab === tabDetail.name && "text-dPri/70"
-                    } flex items-center gap-x-4 `}
+                    } flex items-center gap-x-4 hover:bg-gray-200 dark:hover:bg-gray-800 p-1 md:p-3 rounded-md`}
                 >
                     {tabDetail.icon}
                     <h1>{tabDetail.name}</h1>

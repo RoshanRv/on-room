b0c28967dc2fd6fde1d39b5b8fd95e007258145d
RoshanRv <nkroshankumar@gmail.com>
2023-02-26 20:40:38 +0530

    table comp updated

--- client/components/Table/Table.tsx
+++ client/components/Table/Table.tsx
@@ -7,29 +7,31 @@ interface Props {
 
 const Table = ({ headers, rows }: Props) => {
     return (
-        <table className="w-full divide-y divide-dPri table-fixed text-gray-300 rounded-lg">
+        <table className="w-full divide-y divide-dPri table-fixed text-gray-300 rounded-lg my-4">
             <thead className="bg-dPri/80">
-                {headers.map((head, i) => (
-                    <th
-                        className="py-3 px-6 text-sm  tracking-widest text-left text-gray-700 uppercase dark:text-gray-200 "
-                        key={i}
-                        scope={"col"}
-                    >
-                        {head}
-                    </th>
-                ))}
+                <tr>
+                    {headers.map((head, i) => (
+                        <th
+                            className="py-3 px-6 text-sm  tracking-widest text-left text-gray-700 uppercase dark:text-gray-200 "
+                            key={i}
+                            scope={"col"}
+                        >
+                            {head}
+                        </th>
+                    ))}
+                </tr>
             </thead>
-            <tbody className="bg-white divide-y divide-gray-200 dark:bg-transparent dark:divide-dPri/50">
+            <tbody className="bg-white divide-y  dark:bg-transparent divide-dPri/50">
                 {rows.map((row, rowIndex) => {
                     return (
                         <tr
-                            className="hover:bg-blue-50 dark:hover:bg-gray-700"
+                            className="hover:bg-gray-50 dark:hover:bg-gray-800"
                             key={rowIndex}
                         >
                             {row.map((col, colIndex) => {
                                 return (
                                     <td
-                                        className="py-4 px-6 text-sm font-medium text-gray-900 dark:text-white"
+                                        className="py-4 px-6 text-sm font-medium text-gray-800 dark:text-gray-300 text-clip break-all"
                                         key={colIndex}
                                     >
                                         {col}

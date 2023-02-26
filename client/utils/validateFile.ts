f89902ec9d924293f93102e2f3a293b8460f762b
RoshanRv <nkroshankumar@gmail.com>
2023-02-26 20:41:18 +0530

    validateFile func created

--- client/utils/validateFile.ts
+++ client/utils/validateFile.ts
@@ -1,9 +1,17 @@
 const validTypes = ["pdf", "image"]
 
-const validateFile = (files: FileProps[]) => {
+const validateFile = (files: FileProps[], attachments: FileProps[]) => {
     return files.filter((file) => {
         for (let i of validTypes) {
-            if (file.type.includes(i)) return true
+            if (file.type.includes(i)) {
+                if (
+                    attachments.find(
+                        (assignment) => assignment.name === file.name
+                    )
+                )
+                    return false
+                return true
+            }
         }
         return false
     })

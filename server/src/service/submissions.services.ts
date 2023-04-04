e22de6e6a9efc993b400625c276915ecebc80625
RoshanRv <nkroshankumar@gmail.com>
2023-04-04 19:30:13 +0530

    submission services created

---
+++ server/src/service/submissions.services.ts
@@ -0,0 +1,74 @@
+import { PrismaClient } from "@prisma/client"
+import { updateSubmissionSchemaType } from "@schema/submissions.schema"
+const prisma = new PrismaClient()
+
+export const getSubmissionsByClassroomId = async (classroomId: string) => {
+    try {
+        const submission = await prisma.submission.findMany({
+            where: {
+                assignment: {
+                    classroomId,
+                },
+            },
+            include: {
+                student: true,
+                assignment: true,
+            },
+        })
+
+        return submission
+    } catch (e: any) {
+        throw new Error(e)
+    }
+}
+
+interface submissionsType {
+    filename: string
+    size: number
+    type: string
+    assignmentId: string
+    studentId: string
+}
+
+export const createSubmission = async (data: submissionsType) => {
+    try {
+        const submission = prisma.submission.create({
+            data,
+        })
+        return submission
+    } catch (e: any) {
+        throw new Error(e)
+    }
+}
+
+export const updateSubmission = async (
+    id: string,
+    data: updateSubmissionSchemaType["body"]
+) => {
+    try {
+        const submission = prisma.submission.update({
+            where: {
+                id,
+            },
+            data,
+        })
+
+        return submission
+    } catch (e: any) {
+        throw new Error(e)
+    }
+}
+
+export const deleteSubmission = async (id: string) => {
+    try {
+        const submission = prisma.submission.delete({
+            where: {
+                id,
+            },
+        })
+
+        return submission
+    } catch (e: any) {
+        throw new Error(e)
+    }
+}

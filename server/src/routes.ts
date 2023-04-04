be3470fdb0ffd729ef720ed84def93de49402b82
RoshanRv <nkroshankumar@gmail.com>
2023-04-04 19:28:38 +0530

    endpoints for submission created

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,4 @@
 import { Express, Request, Response } from "express"
-import fileUpload, { FileArray } from "express-fileupload"
 import validateInput from "@middleware/validateInput"
 import {
     createUserSchema,
@@ -9,6 +8,7 @@ import {
 import {
     createUserHandler,
     findStudentsByClassroomHandler,
+    getAllStudentsExceptEnrolledHander,
     updateUserHandler,
 } from "@controller/users.controller"
 import { createSessionSchema } from "@schema/sessions.schema"
@@ -70,13 +70,30 @@ import path from "path"
 import {
     createAttachmentSchema,
     getAttachmentAssignmentIdSchema,
+    getAttachmentByIdSchema,
 } from "@schema/attachments.schema"
 import {
     createAttachmentsHandler,
-    downloadAttachmentHandler,
+    deleteAttachmentByIdHandler,
     getAttachmentAssignmentIdHandler,
+    getAttachmentByIdHandler,
 } from "@controller/attachments.controller"
 import { createAttachments } from "@service/attachments.services"
+import {
+    createSubmissionSchema,
+    deleteSubmissionSchema,
+    getSubmissionsByClassroomIdSchema,
+    getSubmissionSchema,
+    updateSubmissionSchema,
+} from "@schema/submissions.schema"
+import {
+    createSubmissionHandler,
+    deleteSubmissionHandler,
+    getSubmissionByIdHandler,
+    getSubmissionsHandler,
+    updateSubmissionHandler,
+} from "@controller/submissions.controller"
+import sendEmail from "@controller/email.controller"
 
 const routes = (app: Express) => {
     app.get("/healthcheck", (req: Request, res: Response) => {
@@ -100,6 +117,13 @@ const routes = (app: Express) => {
 
     // get current user using token
     app.get("/api/me", requireUser, getCurrentUserHandler)
+
+    // get all students except enrolled
+    app.get(
+        "/api/users/not/enrolled/:classroomId",
+        [requireUser, validateInput(findStudentsByClassroomSchema)],
+        getAllStudentsExceptEnrolledHander
+    )
 
     // create new session / login
     app.post(
@@ -220,38 +244,79 @@ const routes = (app: Express) => {
         deleteAnnouncementHandler
     )
 
-    //          Create New Attachments
-    // app.post(
-    //     "/api/attachment",
-    //     [requireTeacher, validateInput(createAttachmentSchema)],
-    //     createAttachmentsHandler
-    // )
+    //      Attachments
 
-    // app.get("/api/uploads", (req: Request, res: Response) => {
-    //     res.sendFile(path.join(__dirname, "../uploads/Novicers-doc.pdf"))
-
-    //     // res.download(path.join(__dirname, "../uploads/id.jpeg"), (err) => {
-    //     //     if (err) console.log(err)
-    //     // })
-    // })
+    //      get a single attachment(File) using attachmentId
 
     app.get(
-        "/api/attachment/:attachmentId/download",
-        requireUser,
-        downloadAttachmentHandler
+        "/api/attachment/:attachmentId",
+        [requireUser, validateInput(getAttachmentByIdSchema)],
+        getAttachmentByIdHandler
     )
 
+    //     Create Attachements and Upload Files
+
     app.post(
-        "/api/attachment/:assignmentId",
+        "/api/attachment/assignment/:assignmentId",
         [requireTeacher, validateInput(createAttachmentSchema)],
         createAttachmentsHandler
     )
 
+    //  Get All Attachments on a Assignment
+
     app.get(
-        "/api/attachment/:assignmentId",
+        "/api/attachment/assignment/:assignmentId",
         [requireUser, validateInput(getAttachmentAssignmentIdSchema)],
         getAttachmentAssignmentIdHandler
     )
+
+    app.delete(
+        "/api/attachment/:attachmentId",
+        [requireTeacher, validateInput(getAttachmentByIdSchema)],
+        deleteAttachmentByIdHandler
+    )
+
+    //  Submission
+
+    //      get a single Submission(File) using submissionId
+
+    app.get(
+        "/api/submission/:submissionId",
+        [requireUser, validateInput(getSubmissionSchema)],
+        getSubmissionByIdHandler
+    )
+
+    //  fetch all submission of classroomId
+    app.get(
+        "/api/submission/classroom/:classroomId",
+        [requireUser, validateInput(getSubmissionsByClassroomIdSchema)],
+        getSubmissionsHandler
+    )
+
+    //  create a submission for an assignment
+    app.post(
+        "/api/submission/assignment/:assignmentId",
+        [requireStudent, validateInput(createSubmissionSchema)],
+        createSubmissionHandler
+    )
+
+    // update a submission using its ID
+    app.put(
+        "/api/submission/:submissionId",
+        [requireTeacher, validateInput(updateSubmissionSchema)],
+        updateSubmissionHandler
+    )
+
+    // delete a submission using its ID
+    app.delete(
+        "/api/submission/:submissionId",
+        [requireStudent, validateInput(deleteSubmissionSchema)],
+        deleteSubmissionHandler
+    )
+
+    //  Mail....\
+
+    app.post("/api/email/:studentId", requireUser, sendEmail)
 }
 
 export default routes

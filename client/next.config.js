10fb3224db118dbc17a1a1a4b536501797eb473b
RoshanRv <nkroshankumar@gmail.com>
2023-04-15 11:26:24 +0530

    confgured for pwa

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+})
+
+// module.exports = withPWA({
+//     reactStrictMode: true,
+//     images: {
+//         remotePatterns: [
+//             {
+//                 hostname: "*",
+//             },
+//         ],
+//     },
+// })
+
 module.exports = {
     reactStrictMode: true,
     images: {
